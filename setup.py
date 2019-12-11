b7621d936562c83191761fb34ee8e68c0529ca28
Tom Mertens <tomtommertens2@gmail.com>
2019-12-11 14:08:53 +0100

    added xlsxwriter to requirements

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author=about['__author__'],
     license=about['__license__'],
     packages=find_packages(),
-    install_requires=['fastjsonschema', 'click', 'pandas', 'tables'],
+    install_requires=['fastjsonschema', 'click', 'pandas', 'tables', 'xlsxwriter'],
     test_requires=['pytest'],
     python_requires='>=3.6',
     include_package_data=True,
